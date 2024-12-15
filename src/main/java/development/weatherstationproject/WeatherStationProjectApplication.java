a9451ccda97872f2e5442911fb2e149c7bfddad0
Mathais <mathias.egeberg@hotmail.no>
2024-12-15 12:19:22 +0100

    first commit

---
+++ src/main/java/development/weatherstationproject/WeatherStationProjectApplication.java
@@ -0,0 +1,13 @@
+package development.weatherstationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherStationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/development/weatherstationproject/WeatherStationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package development.weatherstationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherStationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
