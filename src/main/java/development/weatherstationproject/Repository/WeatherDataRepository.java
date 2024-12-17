ef0297c4b70885c72d92ef8217f1bd536e65bbfa
Mathais <mathias.egeberg@hotmail.no>
2024-12-17 07:54:08 +0100

    bug fix interface

--- src/main/java/development/weatherstationproject/Repository/WeatherDataRepository.java
+++ src/main/java/development/weatherstationproject/Repository/WeatherDataRepository.java
@@ -3,9 +3,11 @@ package development.weatherstationproject.Repository;
 
 import development.weatherstationproject.entity.WeatherData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@EnableJpaRepositories(basePackages = "development.weatherstationproject.Repository")
 public interface WeatherDataRepository extends JpaRepository<WeatherData, Long> {
     WeatherData findTopByOrderByTimestampDesc(); // Fetch the latest weather data
 }
